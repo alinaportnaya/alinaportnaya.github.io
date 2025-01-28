280951d0c88901b713bf8540d2eab3caf52a5096
alinaportnaya <166427424+alinaportnaya@users.noreply.github.com>
2025-01-28 18:41:59 +0200

    Add files via upload

---
+++ js/app/pages/login.js
@@ -0,0 +1,21 @@
+export const login ={
+    data:function(){
+        return {
+            img:1,
+            hs:0,
+            parent:''
+        }
+    },
+    mounted:function(){
+/*         this.img = this.randomIntFromInterval(1,7); */
+        this.parent = this.$parent.$parent;
+    },
+    methods:{
+
+    },
+    template:`
+    <div class="flex">
+    test
+    </div>
+    `
+};
